87d2b589685d1c5951233ecb1c7da7fcea001fb5
LatifaAlawwad <122611839+LatifaAlawwad@users.noreply.github.com>
2023-12-02 02:05:32 +0300

    web

--- lib/pages/NavigationBarPage.dart
+++ lib/pages/NavigationBarPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'lib/MapView.dart';
+import 'MapView.dart';
 import 'FavoritePage.dart';
 import 'HomePage.dart';
 import 'addpage.dart';
