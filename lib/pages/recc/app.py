0af9ae9d7a6198ce8335777373d4871ef3194d5d
layanAltaleb <layannn00@gmail.com>
2024-05-16 02:17:36 +0300

    update

--- lib/pages/recc/app.py
+++ lib/pages/recc/app.py
@@ -3,16 +3,15 @@ from firebase_admin import credentials, firestore, initialize_app
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import re
+
 app = Flask(__name__)
 stops = {"و", "في", "من", "على", "إلى", "عن", "هذا", "هذه", "هنا", "هناك"}  # Add more stopwords as needed
-stop_word_comp = {"،", "آض", "آمينَ",  "آهاً", "آي", "أ", "أب", "أجل", "أجمع", "أخ", "أخذ", "أصبح", "أضحى", "أقبل", "أقل", "أكثر", "ألا", "أم", "أما", "أمامك", "أمامكَ", "أمسى", "أمّا", "أن", "أنا", "أنت", "أنتم", "أنتما", "أنتن", "أنتِ", "أنشأ", "أنّى", "أو", "أوشك", "أولئك", "أولئكم", "أولاء", "أولالك", "أوّهْ", "أي", "أيا", "أين", "أينما", "أيّ", "أَنَّ", "أََيُّ", "أُفٍّ", "إذ", "إذا", "إذاً", "إذما", "إذن", "إلى", "إليكم", "إليكما", "إليكنّ", "إليكَ", "إلَيْكَ", "إلّا", "إمّا", "إن", "إنّما", "إي", "إياك", "إياكم", "إياكما", "إياكن", "إيانا", "إياه", "إياها", "إياهم", "إياهما", "إياهن", "إياي", "إيهٍ", "إِنَّ", "ا", "ابتدأ", "اثر", "احد", "اخرى", "اخلولق", "اذا", "اربعة", "ارتدّ", "استحال", "اطار", "اعادة", "اعلنت", "اف", "اكثر", "اكد", "الألاء", "الألى", "الا", "الاخيرة", "الان", "الاول", "الاولى", "التى", "التي", "الثاني", "الثانية", "الذاتي", "الذى", "الذي", "الذين", "السابق", "الف", "اللائي", "اللاتي", "اللتان", "اللتيا", "اللتين", "اللذان", "اللذين", "اللواتي", "الماضي", "المقبل", "الوقت", "الى", "اليوم", "اما", "امام", "امس", "ان", "انبرى", "انقلب", "انه", "انها", "او", "اول", "اي", "ايار", "ايام", "ايضا", "ب", "بات", "باسم", "بان", "بخٍ", "برس", "بسبب", "بسّ", "بشكل", "بضع", "بطآن", "بعد", "بعض", "بك", "بكم", "بكما", "بكن", "بل", "بلى", "بما", "بماذا", "بمن", "بن", "بنا", "به", "بها", "بي", "بيد", "بين", "بَسْ", "بَلْهَ", "بِئْسَ", "تانِ", "تانِك", "تبدّل", "تجاه", "تحوّل", "تلقاء", "تلك", "تلكم", "تلكما", "تم", "تينك", "تَيْنِ", "تِه", "تِي", "ثلاثة", "ثم", "ثمّ", "ثمّة", "ثُمَّ", "جعل", "جلل", "جميع", "جير", "حار", "حاشا", "حاليا", "حاي", "حتى", "حرى", "حسب", "حم", "حوالى", "حول", "حيث", "حيثما", "حين", "حيَّ", "حَبَّذَا", "حَتَّى", "حَذارِ", "خلا", "خلال", "دون", "دونك", "ذا", "ذات", "ذاك", "ذانك", "ذانِ", "ذلك", "ذلكم", "ذلكما", "ذلكن", "ذو", "ذوا", "ذواتا", "ذواتي", "ذيت", "ذينك", "ذَيْنِ", "ذِه", "ذِي", "راح", "رجع", "رويدك", "ريث", "رُبَّ", "زيارة", "سبحان", "سرعان", "سنة", "سنوات", "سوف", "سوى", "سَاءَ", "سَاءَمَا", "شبه", "شخصا", "شرع", "شَتَّانَ", "صار", "صباح", "صفر", "صهٍ", "صهْ", "ضد", "ضمن", "طاق", "طالما", "طفق", "طَق", "ظلّ", "عاد", "عام", "عاما", "عامة", "عدا", "عدة", "عدد", "عدم", "عسى", "عشر", "عشرة", "علق", "على", "عليك", "عليه", "عليها", "علًّ", "عن", "عند", "عندما", "عوض", "عين", "عَدَسْ", "عَمَّا", "غدا", "غير", "ـ", "ف", "فان", "فلان", "فو", "فى", "في", "فيم", "فيما", "فيه", "فيها", "قال", "قام", "قبل", "قد", "قطّ", "قلما", "قوة", "كأنّما", "كأين", "كأيّ", "كأيّن", "كاد", "كان", "كانت", "كذا", "كذلك", "كرب", "كل", "كلا", "كلاهما", "كلتا", "كلم", "كليكما", "كليهما", "كلّما", "كلَّا", "كم", "كما", "كي", "كيت", "كيف", "كيفما", "كَأَنَّ", "كِخ", "لئن", "لا", "لات", "لاسيما", "لدن", "لدى", "لعمر", "لقاء", "لك", "لكم", "لكما", "لكن", "لكنَّما", "لكي", "لكيلا", "للامم", "لم", "لما", "لمّا", "لن", "لنا", "له", "لها", "لو", "لوكالة", "لولا", "لوما", "لي", "ليت", "ليس", "ليسا", "ليست", "ليستا", "ليسوا", "ما", "ماانفك", "مابرح", "مادام", "ماذا", "مازال", "مافتئ", "مايو", "متى", "مثل", "مذ", "مساء", "مع", "معاذ", "مقابل", "مكانكم", "مكانكما", "مكانكنّ", "مكانَك", "مليار", "مليون", "مما", "ممن", "من", "منذ", "منها", "مه", "مهما", "مَنْ", "نحن", "نحو", "نعم", "نفس", "نفسه", "نهاية", "نَخْ", "نِعِمّا", "نِعْمَ", "ها", "هاؤم", "هاكَ", "هاهنا", "هبّ", "هذا", "هذان", "هذه", "هذي", "هذين", "هكذا", "هل", "هلمَّ", "هلّا", "هم", "هما", "هن", "هنا", "هناك", "هنالك", "هو", "هؤلاء", "هي", "هيا", "هيت", "هيّا", "هَؤلاء", "هَاتان", "هَاته", "هَاتي", "هَاتَيْن", "هَاكَ", "هَذا", "هَذان", "هَذَيْن", "هَذِه", "هَذِي", "هَذَيْن", "هَيْهَاتَ", "و", "و6", "وا", "واحد", "واضاف", "واضافت", "واكد", "وان", "واها", "واوضح", "وراءك", "وفي", "وقال", "وقالت", "وقد", "وقف", "وكان", "وكانت", "ولا", "ولم", "ومن", "من", "وهو", "وهي", "ويكأنّ", "وَيْ", "وُشْكَانَ", "يكون", "يمكن", "يوم", "ّأيّان"}
 
-# Initialize Firebase
-# Initialize Firebase
 # Initialize Firebase
 cred = credentials.Certificate("fir-hail-ec5a7-firebase-adminsdk-9d92w-d67984762b.json")
 initialize_app(cred)
 db = firestore.client()
+
 
 # Function to fetch user data from Firestore
 def fetch_user_data(user_id):
@@ -29,6 +28,7 @@ def fetch_user_data(user_id):
 
     return favorites, preferences
 
+
 # Function to fetch approved places from Firestore
 def fetch_approved_places(city_name=None):
     places_ref = db.collection('ApprovedPlaces')
@@ -38,6 +38,7 @@ def fetch_approved_places(city_name=None):
         places = [place for place in places if place.get('city', '') == city_name]
 
     return places
+
 
 # Preprocess data
 def preprocess_data(approved_places):
@@ -62,6 +63,7 @@ def preprocess_data(approved_places):
 
     return item_profiles
 
+
 # Preprocess text
 def preprocess_text(text):
     # Tokenize Arabic text
@@ -74,12 +76,13 @@ def preprocess_text(text):
     lowercase_tokens = [token.lower() for token in arabic_only]
 
     # Remove stopwords
-    cleaned_tokens = [token for token in lowercase_tokens if token not in stops and token not in stop_word_comp and len(token) >= 2]
+    cleaned_tokens = [token for token in lowercase_tokens if token not in stops and len(token) >= 2]
 
     # Join tokens back into text
     preprocessed_text = ' '.join(cleaned_tokens)
 
     return preprocessed_text
+
 
 # Function to get place name
 def get_place_name(approved_places, place_id, city_name):
@@ -89,6 +92,7 @@ def get_place_name(approved_places, place_id, city_name):
             place_name = place.get('placeName', '')
             return place_name
     return "Unknown"
+
 
 # Function to preprocess user preferences
 def preprocess_preferences(preferences):
@@ -113,8 +117,7 @@ def preprocess_preferences(preferences):
 
     return preference_profiles
 
-# Function to recommend places based on item similarity
-# Function to recommend places based on item similarity
+
 # Function to recommend places based on item similarity
 def model(user_favorites, preference_profiles, item_profiles):
     recommendations = set()
@@ -181,5 +184,7 @@ def get_recommendations():
             recommended_ids.add(place_id)  # Add the place ID to the set of recommended IDs
 
     return jsonify({'api': recommended_places})
-if __name__ == '_main_':
+
+
+if __name__ == '__main__':
     app.run(debug=True)
